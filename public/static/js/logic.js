f508db46eb88befd2c4b3f1e210a599bdc67c34c
Rakshak R.Hegde <rakshakhegde@gmail.com>
2016-03-05 18:04:10 +0530

    initial commit

---
+++ public/static/js/logic.js
@@ -0,0 +1,42 @@
+rootRef = new Firebase('https://instavans.firebaseio.com/');
+rootRef.onAuth(refresh);
+
+function setFields() {
+	var uid = rootRef.getAuth().uid;
+	rootRef.child('user/' + uid).once('value', function(data) {
+		$('#logged-in-section').show();
+	});
+}
+
+$('#login').click(function(event) {
+	rootRef.authWithOAuthPopup('twitter');
+});
+
+$('#logout').click(function(event) {
+	rootRef.unauth();
+});
+
+function refresh(authData) {
+	$('.row, #error-text').hide();
+	if (authData) {
+		$('#logged-in-section').show();
+	} else {
+		$('#login-section').show();
+	}
+}
+
+function invalidTokens() {
+	showError('Invalid tokens provided in URL!');
+}
+
+function showError(errorMsg) {
+	$('#error-text').show();
+	$('#error-text').append('</br>' + errorMsg);
+}
+
+function makeUrlInput(pos, txt) {
+	return '<div class="input-field col s12 m10 offset-m1 l8 offset-l2">\
+		<input id="input-' + pos + '" type="text" class="url-input" oninput="changed(' + pos + ')" value="' + txt + '"/>\
+		<label for="input-' + pos + '">Amazon Product URL</label>\
+		</div>';
+}
